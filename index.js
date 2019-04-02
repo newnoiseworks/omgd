aa992b528afcd9545a1ffa8bd72172a239c34aba
Noise <hello@newnoiseworks.com>
2019-04-02 16:21:41 -0400

    working local and staging build processes

--- index.js
+++ index.js
@@ -4,74 +4,176 @@ const util = require('util')
 const exec = util.promisify(require('child_process').exec);
 const AdmZip = require('adm-zip')
 
+const COMMAND = process.argv[2]
+const ENVIRONMENT = process.argv[3]
+const VERSION = process.argv[4]
+
+const IS_LOCAL = ENVIRONMENT === "local"
+
+const LAUNCHER_DIR = process.cwd() + "/../launcher"
+const WEBSITE_DIR = process.cwd() + "/../website"
+const GAME_DIR = process.cwd() + "/../game"
+
 const TMP_DIR = process.cwd() + "/.tmp-tpl-fred/"
-const LAUNCHER_DIR = process.cwd() + "../launcher"
-const WEBSITE_DIR = process.cwd() + "../website"
-const GAME_DIR = process.cwd() + "../game"
+const TMP_LAUNCHER_DIR = TMP_DIR + "launcher"
+const TMP_WEBSITE_DIR = TMP_DIR + "website"
+const TMP_GAME_DIR = TMP_DIR + "game"
+
+const LAUNCHER_REPO = "git@github.com:newnoiseworks/tpl-launcher.git"
+const WEBSITE_REPO = "git@github.com:newnoiseworks/tpl-website.git"
+const GAME_REPO = "git@github.com:newnoiseworks/not-stardew.git"
 
 const LOCAL_GODOT_WIN_BINARY = process.cwd() + "/../Godot/Godot.exe"
 const GODOT_WIN_DOWNLOAD_URL = "https://downloads.tuxfamily.org/godotengine/3.1/mono/Godot_v3.1-stable_mono_win64.zip"
 
-const command = process.argv[2]
-const environment = process.argv[3]
-const version = process.argv[4]
-
-const is_local = environment === "local"
-
-function setup() {
-  setupTempWorkingDirectory()
+async function setup() {
+  console.log("setting up...")
+  await setupTempWorkingDirectory()
 }
 
-function setupTempWorkingDirectory() {
+async function setupTempWorkingDirectory() {
   if (fs.existsSync(TMP_DIR))
     rimraf.sync(TMP_DIR)
  
   fs.mkdirSync(TMP_DIR)
 
-  if (is_local === false) {
-    process.chdir(TMP_DIR)
-  }
+  if (IS_LOCAL) return
+
+  process.chdir(TMP_DIR)
+
+  console.log("cloning repos...")
+  await cloneRepositoriesFromGithub()
+}
+
+async function cloneRepositoriesFromGithub() {
+  // TODO: abort if "git status" shows changes
+  await Promise.all([{
+    originalRepoDir: LAUNCHER_DIR,
+    originalRepo: LAUNCHER_REPO,
+    tmpRepoDir: TMP_LAUNCHER_DIR,
+  },{
+    originalRepoDir: WEBSITE_DIR,
+    originalRepo: WEBSITE_REPO,
+    tmpRepoDir: TMP_WEBSITE_DIR,
+  },/*{
+    originalRepoDir: GAME_DIR,
+    originalRepo: GAME_REPO,
+    tmpRepoDir: TMP_GAME_DIR,
+  }*/].map((proj) => {
+    let launcherVersion
+    process.chdir(proj.originalRepoDir)
+    
+    return exec(`git rev-parse HEAD`)
+    .then(({ stderr, stdout }) => {
+      launcherVersion = stdout.trim()
+    }).then(() => {
+      process.chdir(TMP_DIR)
+      return exec(`git clone --depth 1 ${proj.originalRepo} ${proj.tmpRepoDir}`)
+    }).then(() => {
+      if (ENVIRONMENT !== "production") {
+        process.chdir(proj.tmpRepoDir)
+        return exec(`git reset --hard ${launcherVersion}`)
+      }
+    })
+  }))
 }
 
 async function build() {
   const original_path = process.cwd()
+  const gameDir = GAME_DIR
+  const websiteDir = IS_LOCAL ? WEBSITE_DIR : TMP_WEBSITE_DIR
+  const launcherDir = IS_LOCAL ? LAUNCHER_DIR : TMP_LAUNCHER_DIR
 
-  console.log("building godot project...")
-  process.chdir(GAME_DIR)
-  await exec(`${LOCAL_GODOT_WIN_BINARY} --export "windows-${environment}" ${TMP_DIR}/game/tpl.exe --no-window`)
+  console.log("Building godot game client...")
+
+  process.chdir(gameDir)
+
+  if (IS_LOCAL === false) {
+    // check git status in staging, git status -s should return empty
+    const statusString = (await exec(`git status -s`)).stdout.trim()
+
+    if (!!statusString) throw new Error(`Error! can't build to ${ENVIRONMENT} with unchecked file changes in the game folder. Try git stashing first.`)
+
+    // TODO: in production check current git branch && commit in dir -- should be master && latest
+  }
+
+  try {
+    await exec(`${LOCAL_GODOT_WIN_BINARY} --export "windows-${ENVIRONMENT}" ${TMP_DIR}gameBuild/tpl.exe --no-window`)
+  } catch(err) {
+    console.log("!!!")
+    console.log(err)
+    throw new Error(err)
+  }
 
   console.log("compressing godot executables...")
   process.chdir(original_path)
   const gameZip = new AdmZip();
-  gameZip.addLocalFolder(TMP_DIR + "/game")
-  gameZip.writeZip(TMP_DIR + "/tpl-win.zip")
+  gameZip.addLocalFolder(TMP_DIR + "gameBuild")
+  gameZip.writeZip(TMP_DIR + "tpl-win.zip")
 
-  // TODO: copy .zip to launcher 
-  fs.copyFileSync(TMP_DIR + "/tpl-win.zip", LAUNCHER_DIR + "/tpl-win.zip")
+  console.log("copying godot zip to launcher...")
+  fs.copyFileSync(TMP_DIR + "tpl-win.zip", launcherDir + "/tpl-win.zip")
   
-  // TODO: copy .zip to website repo w/ appropriate version in filename
-    // ??? How do I get that version? most recent tag?
+  let versionString
+  if (!!VERSION)
+    versionString = VERSION
+  else {
+    process.chdir(gameDir)
+    await exec("git fetch --tags")
+    // ??? For non production builds, do we need to adjust build #? Or should we only worry about that for prod pushes? We've gotta remember to update all the /game files manually as well. This really only matters for testing & for triggering updates w/n the launcher so probably not necessary, the only requirement is that all  version #'s are in sync. may be worth automating the references w/n Godot and Nakama somehow as well.
+    versionString = (await exec("git describe --tags --abbrev=0")).stdout.trim()
+    process.chdir(original_path)
+  }
+
+  console.log("copying godot zip to website...")
+  fs.copyFileSync(TMP_DIR + "tpl-win.zip", websiteDir + `/public/static/ThePromisedLand-${versionString}.win.zip`)
+
+  console.log("building website for firebase...")
+  process.chdir(websiteDir)
+
+  if (IS_LOCAL === false)
+    await exec("npm install --no-progress")
+
+  await exec("npm run build")
+
+  if (IS_LOCAL === false) {
+    //console.log("packaging launcher...")
+    //process.chdir(launcherDir)
+    //await exec(`yarn && yarn package`)
+
+    // TODO: zip up necessary install files for launcher windows app
+    // TODO: move .zip for launcher to website w/ launcher version number in name
+  }
 }
 
-function deploy() {
+async function deploy() {
   console.log("running deploy...")
+
+  if (IS_LOCAL) return
+
+  process.chdir(TMP_WEBSITE_DIR)
+  await exec(`firebase deploy`)
 }
 
-function buildAndDeploy() {
+async function buildAndDeploy() {
   console.log("running build and deploy...")
+  await build()
+  await deploy()
 }
 
 // eventually the file should be just the below and reference modules probably
-setup()
-
-switch(command) {
-  case "build":
-    build()
-    break
-  case "deploy":
-    deploy()
-    break
-  case "build-and-deploy":
-    buildAndDeploy()
-    break
-}
+(async function() {
+  await setup()
+  
+  switch(COMMAND) {
+    case "build":
+      await build()
+      break
+    case "deploy":
+      deploy()
+      break
+    case "build-and-deploy":
+      buildAndDeploy()
+      break
+  }
+})()

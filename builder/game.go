c3c851288e7105d43ff44f81bedcb1312625379f
Anand Mallik <noise@Anands-MacBook-Pro.local>
2020-08-18 14:22:30 -0400

    game build destroys appropo images and uses utils command

--- builder/game.go
+++ builder/game.go
@@ -3,11 +3,11 @@ package builder
 import (
 	"fmt"
 	"log"
-	"os/exec"
 	"path/filepath"
 
 	"github.com/logrusorgru/aurora"
 	"github.com/newnoiseworks/tpl-fred/builder/config"
+	"github.com/newnoiseworks/tpl-fred/utils"
 )
 
 // BuildGame doinit
@@ -23,49 +23,23 @@ func BuildGame(environment string, buildPath string) {
 	buildDistro("mac", environment, buildPath, path)
 	buildDistro("windows", environment, buildPath, path)
 	buildDistro("x11", environment, buildPath, path)
-	destroyDockerImages(buildPath, path)
 }
 
 func buildDistro(target string, environment string, buildPath string, gamePath string) {
 	cmdStr := fmt.Sprintf("BUILD_ENV=%s docker-compose run build-%s", environment, target)
-	cmd := exec.Command("/bin/sh", "-c", cmdStr)
-	cmd.Dir = gamePath
 
 	fmt.Print(aurora.Cyan(fmt.Sprintf("Building %s distro...\n", target)))
 
-	out, err := cmd.Output()
-
-	if err != nil {
-		fmt.Printf("%s", out)
-		log.Fatal(aurora.Red(fmt.Sprintf("Error building %s distribution using command %s \n", target, cmdStr)))
-		return
-	}
+	utils.CmdOnDir(cmdStr, fmt.Sprintf("building %s distro using command %s \n", target, cmdStr), gamePath)
 
 	fmt.Print(aurora.Green(fmt.Sprintf("%s distro built!\n", target)))
+
+	destroyDockerImage(environment, buildPath, gamePath, target)
 }
 
-func destroyDockerImages(buildPath string, gamePath string) {
-	cmdStr := "docker-compose down --rmi=all"
-	cmd := exec.Command("/bin/sh", "-c", cmdStr)
-	cmd.Dir = gamePath
+func destroyDockerImage(environment string, buildPath string, gamePath string, distro string) {
+	dockerImgName := fmt.Sprintf("newnoiseworks/game-build-%s-%s", distro, environment)
+	cmdStr := fmt.Sprintf("docker rmi -f %s", dockerImgName)
 
-	path, err := filepath.Abs(fmt.Sprintf("%s/game", buildPath))
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-
-	cmd.Dir = path
-
-	fmt.Print(aurora.Cyan("Destroying docker images...\n"))
-
-	out, err := cmd.Output()
-
-	if err != nil {
-		fmt.Printf("%s", out)
-		log.Fatal(aurora.Red("Error destroying images"))
-		return
-	}
-
-	fmt.Print(aurora.Magenta("Docker images destroyed\n"))
+	utils.CmdOnDir(cmdStr, fmt.Sprintf(" destroying docker image post build for %s", distro), gamePath)
 }

a08f5004813146cb5ae6b25053b0705452b80704
Anand Mallik <amallik@newnoiseworks.com>
2021-05-03 16:06:51 -0400

    struct less mission and item files

--- utils/items.go
+++ utils/items.go
@@ -7,31 +7,9 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-type priceItem struct {
-	Item     string `yaml:"item"`
-	Quantity int    `yaml:"quantity"`
-}
-
-type item struct {
-	Key                string      `yaml:"key"`
-	Sale               []priceItem `yaml:"sale"`
-	Price              []priceItem `yaml:"price"`
-	PurchaseValidation bool        `yaml:"purchase_validation"`
-	ComboPrice         bool        `yaml:"combo_price"`
-	ToPlant            string      `yaml:"toPlant"`
-	FromSeeds          string      `yaml:"fromSeeds"`
-	GrowthStages       []int       `yaml:"growthStages"`
-	MaxYield           int         `yaml:"maxYield"`
-}
-
-// InventoryItems docs
-type InventoryItems struct {
-	Items []item `yaml:"items"`
-}
-
 // GetItems d
-func GetItems() InventoryItems {
-	c := InventoryItems{}
+func GetItems() map[interface{}]interface{} {
+	c := make(map[interface{}]interface{})
 
 	yamlFile, err := ioutil.ReadFile("resources/items.yml")
 	if err != nil {

04cbb989df808a7fc95646e8490ace62529a06c8
Anand Mallik <amallik@newnoiseworks.com>
2021-05-09 13:36:20 -0400

    tries to execute simple command

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 use structopt::StructOpt;
 
+mod run_cmd_on_dir;
 mod repo_resource_fetcher;
-
-// omgd new
-// omgd generate thing [argsAsSplat]
 
 #[derive(StructOpt)]
 struct Cli {
@@ -13,10 +11,21 @@ struct Cli {
 
 #[derive(StructOpt)]
 enum Command {
-    /// Spawns a new project yall
+    /// Spawns a new project
     New {
         /// The name of the project, will create a folder in this name in the current directory
         name: String,
+    },
+    /// Generate various code
+    Codegen {
+        /// What you want to generate
+        plan: String,
+    },
+    /// Start the server
+    ServerStart {
+        /// Set to tail logs, will print output from server to this terminal
+        #[structopt(short = "t", long = "tail")]
+        tail: bool,
     },
 }
 
@@ -26,6 +35,17 @@ fn main() {
     match args.commands {
         Command::New { name } => {
             repo_resource_fetcher::get_directory(name)
+        }
+        Command::Codegen { plan } => {
+            println!("We will generate code from plan {}.", plan)
+        }
+        Command::ServerStart { tail } => {
+            println!("Start that server");
+            if tail {
+                run_cmd_on_dir::run("docker-compose up", "start them servers", "server", true);
+            } else {
+                run_cmd_on_dir::run("docker-compose up -d", "start them servers", "server", true);
+            }
         }
     }
 }
--- src/repo_resource_fetcher/mod.rs
+++ src/repo_resource_fetcher/mod.rs
@@ -1,7 +1,9 @@
 pub fn get_directory(record_as: String) {
     if cfg!(debug_assertions) {
         // get the directory from the local filesystem
+        println!("Recording to directory {} from local filesystem", record_as)
     } else {
+        println!("Recording to directory {} from github repo", record_as)
         // get the directory from github.com
     }
 }

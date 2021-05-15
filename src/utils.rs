f9b10223adfc06002a8557e3fd1a1a878846081e
Anand Mallik <amallik@newnoiseworks.com>
2021-05-15 12:49:05 -0400

    condensing

--- src/utils.rs
+++ src/utils.rs
@@ -57,17 +57,13 @@ pub fn get_directory_from_repo(sub_folder: &str, target_path: &str) {
 
     // This can't be good for windows?
     run_cmd_on_dir(&mk_repo_dir_cmd, "making temp repo as needed", ".");
-
     run_cmd_on_dir("git init", "setting up git repo in temp folder", &repo_dir);
-
     run_cmd_on_dir("git config core.sparseCheckout true", "setting folder to sparse checkout", &repo_dir);
 
     let sparse_checkout_path = format!("{}/.git/info/sparse_checkout", repo_dir);
-
     fs::write(sparse_checkout_path, sub_folder).expect("Unable to write sparse checkout file");
 
     let setup_origin = format!("git remote add -f origin {}", repo);
-
     run_cmd_on_dir(&setup_origin, "setting up sparse-checkout file", &repo_dir);
 
     let git_pull_cmd;
@@ -82,11 +78,9 @@ pub fn get_directory_from_repo(sub_folder: &str, target_path: &str) {
     run_cmd_on_dir(&git_pull_cmd, "getting files from repo", &repo_dir);
 
     let final_move = format!("mv {}/{} {}", repo_dir, sub_folder, target_path);
-
     run_cmd_on_dir(&final_move, "moving folder", ".");
 
     let final_cleanup = format!("rm -rf {}", repo_dir);
-
     run_cmd_on_dir(&final_cleanup, "cleaning up original folder", ".");
 }
 

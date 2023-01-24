82cba9536a97801732d2f65588e00c76ac27d288
New Noise Anand <amallik@newnoiseworks.com>
2023-01-23 21:31:03 -0500

    build templates accepts a default

--- cmd/buildTemplates.go
+++ cmd/buildTemplates.go
@@ -33,7 +33,11 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		utils.BuildTemplatesFromPath(args[0], Profile, OutputDir, templateExtension, Verbosity)
+		if len(args) == 0 {
+			utils.BuildTemplatesFromPath(".", Profile, OutputDir, templateExtension, Verbosity)
+		} else {
+			utils.BuildTemplatesFromPath(args[0], Profile, OutputDir, templateExtension, Verbosity)
+		}
 	},
 }
 

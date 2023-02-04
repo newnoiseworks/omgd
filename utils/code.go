36171e27cb189854a67186d5b0d37e8b052bed90
New Noise Anand <amallik@newnoiseworks.com>
2023-02-03 22:16:14 -0500

    adds codegen command for example 2d player movement

--- utils/code.go
+++ utils/code.go
@@ -7,22 +7,31 @@ import (
 )
 
 type CodeGenerationPlan struct {
-	OutputDir string
-	Target    string
-	Plan      string
-	Verbosity bool
+	OutputDir   string
+	Target      string
+	Plan        string
+	Verbosity   bool
+	SkipCleanup bool
 }
 
+// generates code per plan
 func (cp *CodeGenerationPlan) Generate() {
-	outputPath := fmt.Sprintf("%s/%s", cp.OutputDir, cp.Target)
-
 	switch cp.Plan {
 	case "new":
-		cp.generateNew(outputPath)
+		cp.generateNew()
+	case "example-2d-player-movement":
+		cp.generateExample2DPlayerMovement()
+	}
+
+	// optionally skip cleanup to observe files, mostly for testing
+	if cp.SkipCleanup == false {
+		cp.Cleanup()
 	}
 }
 
-func (cp *CodeGenerationPlan) generateNew(outputPath string) {
+// generates code needed for new projects
+func (cp *CodeGenerationPlan) generateNew() {
+	outputPath := fmt.Sprintf("%s/%s", cp.OutputDir, cp.Target)
 	err := CopyStaticDirectory("static/new", outputPath)
 
 	if err != nil {
@@ -32,8 +41,6 @@ func (cp *CodeGenerationPlan) generateNew(outputPath string) {
 	newProfile := GetProfile(fmt.Sprintf("%s/profiles/local", outputPath))
 	newProfile.UpdateProfile("game.name", cp.Target)
 
-	err = os.Mkdir(fmt.Sprintf("%s/resources", outputPath), 0755)
-
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -42,7 +49,60 @@ func (cp *CodeGenerationPlan) generateNew(outputPath string) {
 		fmt.Sprintf("%s/profiles/local", outputPath),
 		outputPath,
 		"newomgdtpl",
+		!cp.SkipCleanup,
+		cp.Verbosity,
+	)
+}
+
+// generates example 2d player movement code
+func (cp *CodeGenerationPlan) generateExample2DPlayerMovement() {
+	tmpDir := fmt.Sprintf("%s/.omgdtmp", cp.OutputDir)
+
+	err := CopyStaticDirectory("static/example-2d-player-movement", tmpDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	newProfile := GetProfile(fmt.Sprintf("%s/profiles/local", tmpDir))
+	newProfile.UpdateProfile("omgd.channel_name", cp.Target)
+
+	BuildTemplatesFromPath(
+		fmt.Sprintf("%s/profiles/local", tmpDir),
+		tmpDir,
+		"omgdtpl",
 		true,
 		cp.Verbosity,
 	)
+}
+
+// cleanup code
+func (cp *CodeGenerationPlan) Cleanup() {
+	switch cp.Plan {
+	// case "new":
+	case "example-2d-player-movement":
+		cp.cleanupExample2DPlayerMovement()
+	}
+}
+
+// cleans up example 2d player movement code
+func (cp *CodeGenerationPlan) cleanupExample2DPlayerMovement() {
+	tmpDir := fmt.Sprintf("%s/.omgdtmp", cp.OutputDir)
+
+	err := os.RemoveAll(fmt.Sprintf("%s/profiles", tmpDir))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = CopyStaticDirectory(tmpDir, cp.OutputDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = os.RemoveAll(tmpDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// TODO: use without this, then if something goes wrong, try it
+	// 3. build templates across entire project (for some reason? not sure how to test / prove this / why it happens tbh)
 }
--- utils/code_test.go
+++ utils/code_test.go
@@ -1,11 +1,13 @@
 package utils
 
 import (
+	"io/ioutil"
 	"os"
+	"regexp"
 	"testing"
 )
 
-// tests generation of new projects
+// tests generation of new godot projects
 func TestCodeGenCmdNewProjectWritesAndCleansUpFiles(t *testing.T) {
 	t.Cleanup(func() {
 		err := os.RemoveAll("static/test/newProject")
@@ -37,4 +39,78 @@ func TestCodeGenCmdNewProjectWritesAndCleansUpFiles(t *testing.T) {
 	if !os.IsNotExist(err) {
 		t.Fatal("Templates didn't cleanup afterwards")
 	}
+}
+
+// tests generation of godot example 2d player movement project
+func TestCodeGenCmdExample2DPlayerMovement(t *testing.T) {
+	t.Cleanup(func() {
+		err := os.RemoveAll("static/test/newProject")
+
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	// generates a new project to work in
+	newProjectCodePlan := CodeGenerationPlan{
+		OutputDir: "static/test",
+		Target:    "newProject",
+		Plan:      "new",
+	}
+
+	newProjectCodePlan.Generate()
+
+	codePlan := CodeGenerationPlan{
+		OutputDir:   "static/test/newProject",
+		Plan:        "example-2d-player-movement",
+		Target:      "movement",
+		SkipCleanup: true,
+	}
+
+	codePlan.Generate()
+
+	localProfile := GetProfile("static/test/newProject/.omgdtmp/profiles/local")
+	expected := "movement"
+	received := localProfile.Get("omgd.channel_name")
+
+	if expected != received {
+		testLogComparison(expected, received)
+
+		t.Fatalf("Profile didn't update with channel name in tmp folder")
+	}
+
+	file, err := ioutil.ReadFile("static/test/newProject/.omgdtmp/game/Character/CharacterController.gd")
+	if err != nil {
+		t.Fatalf("Cannot find file: %s\n", err)
+	}
+
+	matches, err := regexp.Match(`MovementEvent`, file)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !matches {
+		t.Fatalf("build-templates didn't adjust static/test/newProject/.omgdtmp/game/CharacterController.gd")
+	}
+
+	_, err = os.Stat("static/test/newProject/game/profiles/local.yml")
+	if os.IsNotExist(err) == false {
+		t.Fatal("Profiles folder didn't cleanup afterwards")
+	}
+
+	codePlan.Cleanup()
+
+	_, err = os.Stat("static/test/newProject/game/Character/CharacterController.gd")
+	if os.IsNotExist(err) {
+		t.Fatal("Files were not moved into main project folder post cleanup")
+	}
+
+	_, err = os.Stat("static/test/newProject/.omgdtmp")
+	if !os.IsNotExist(err) {
+		t.Fatal("Temporary folder was not cleaned up")
+	}
+}
+
+// tests generation of omgd channels
+func TestCodeGenCmdOMGDChannelCreation(t *testing.T) {
+
 }

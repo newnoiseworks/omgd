4c1c47c78a5cd11d6d8d02023b0320faed12bb5b
New Noise Anand <amallik@newnoiseworks.com>
2023-02-04 19:13:29 -0500

    sets up a test, still needs to fill out rest, resets tmp dir before generating new code

--- utils/code_test.go
+++ utils/code_test.go
@@ -60,6 +60,7 @@ func TestCodeGenCmdExample2DPlayerMovement(t *testing.T) {
 
 	newProjectCodePlan.Generate()
 
+	// generates code plan for 2d movement, skips cleanup for testing
 	codePlan := CodeGenerationPlan{
 		OutputDir:   "static/test/newProject",
 		Plan:        "example-2d-player-movement",
@@ -73,17 +74,20 @@ func TestCodeGenCmdExample2DPlayerMovement(t *testing.T) {
 	expected := "movement"
 	received := localProfile.Get("omgd.channel_name")
 
+	// check to see if profile was edited
 	if expected != received {
 		testLogComparison(expected, received)
 
 		t.Fatalf("Profile didn't update with channel name in tmp folder")
 	}
 
+	// check to see if buildTemplates created new file
 	file, err := ioutil.ReadFile("static/test/newProject/.omgdtmp/game/Character/CharacterController.gd")
 	if err != nil {
 		t.Fatalf("Cannot find file: %s\n", err)
 	}
 
+	// check to see if templates were properly edited w/ info
 	matches, err := regexp.Match(`MovementEvent`, file)
 	if err != nil {
 		t.Fatal(err)
@@ -92,18 +96,21 @@ func TestCodeGenCmdExample2DPlayerMovement(t *testing.T) {
 		t.Fatalf("build-templates didn't adjust static/test/newProject/.omgdtmp/game/CharacterController.gd")
 	}
 
-	_, err = os.Stat("static/test/newProject/game/profiles/local.yml")
+	codePlan.Cleanup()
+
+	// check to make sure profiles dir in tmp folder cleaned up
+	_, err = os.Stat("static/test/newProject/.omgdtmp/profiles/local.yml")
 	if os.IsNotExist(err) == false {
 		t.Fatal("Profiles folder didn't cleanup afterwards")
 	}
 
-	codePlan.Cleanup()
-
+	// check to make sure templates were moved into main folder
 	_, err = os.Stat("static/test/newProject/game/Character/CharacterController.gd")
 	if os.IsNotExist(err) {
 		t.Fatal("Files were not moved into main project folder post cleanup")
 	}
 
+	// make sure .omgdtmp folder is cleaned up
 	_, err = os.Stat("static/test/newProject/.omgdtmp")
 	if !os.IsNotExist(err) {
 		t.Fatal("Temporary folder was not cleaned up")
@@ -112,5 +119,56 @@ func TestCodeGenCmdExample2DPlayerMovement(t *testing.T) {
 
 // tests generation of omgd channels
 func TestCodeGenCmdOMGDChannelCreation(t *testing.T) {
+	t.Cleanup(func() {
+		err := os.RemoveAll("static/test/newProject")
 
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	// generates a new project to work in
+	newProjectCodePlan := CodeGenerationPlan{
+		OutputDir: "static/test",
+		Target:    "newProject",
+		Plan:      "new",
+	}
+
+	newProjectCodePlan.Generate()
+
+	// generates channel code within new project
+	codePlan := CodeGenerationPlan{
+		OutputDir:   "static/test/newProject",
+		Plan:        "channel",
+		Target:      "match_channel",
+		SkipCleanup: true,
+	}
+
+	codePlan.Generate()
+
+	// checks to make sure local.yml in tmp folder gets updated
+	localProfile := GetProfile("static/test/newProject/.omgdtmp/profiles/local")
+	expected := "match_channel"
+	received := localProfile.Get("omgd.channel_name")
+
+	if expected != received {
+		testLogComparison(expected, received)
+
+		t.Fatalf("Profile didn't update with channel name in tmp folder")
+	}
+
+	// checks to make sure templates were created and properly renamed
+	file, err := ioutil.ReadFile("static/test/newProject/.omgdtmp/game/Autoloads/MatchChannelManager.gd")
+	if err != nil {
+		t.Fatalf("Cannot find file: %s\n", err)
+	}
+
+	// makes sure templates were adjusted with proper variables
+	matches, err := regexp.Match(`match_channel`, file)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !matches {
+		t.Fatalf("build-templates didn't adjust static/test/newProject/.omgdtmp/game/Autoloads/MatchChannelManager.gd")
+	}
 }

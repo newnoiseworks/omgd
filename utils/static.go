0273ece4b0bc74121185f0d28e37acf2ea844e18
New Noise Anand <amallik@newnoiseworks.com>
2023-02-06 03:02:36 -0500

    adds ways to adjust file contents with string replacements, adjust file names as well on copy

--- utils/static.go
+++ utils/static.go
@@ -5,12 +5,24 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 //go:embed static/*
 var staticFiles embed.FS
 
 const FILE_WRITE_PERMS = 0755
+
+type StaticCodeFilePathAlteration struct {
+	filePathToRead          string
+	filePathToWrite         string
+	stringToReadForReplace  string
+	stringToWriteForReplace string
+}
+
+type StaticCodeCopyPlan struct {
+	filePathAlterations []StaticCodeFilePathAlteration
+}
 
 func GetStaticFile(file string) (string, error) {
 	byteArr, err := staticFiles.ReadFile(file)
@@ -20,7 +32,7 @@ func GetStaticFile(file string) (string, error) {
 	return retVal, err
 }
 
-func CopyStaticDirectory(pathToCopy string, pathToCopyTo string) error {
+func (sccp *StaticCodeCopyPlan) CopyStaticDirectory(pathToCopy string, pathToCopyTo string) error {
 	files, err := staticFiles.ReadDir(pathToCopy)
 	if err != nil {
 
@@ -38,44 +50,100 @@ func CopyStaticDirectory(pathToCopy string, pathToCopyTo string) error {
 
 	for i := 0; i < len(files); i++ {
 		file := files[i]
+		filePathToRead := fmt.Sprintf("%s/%s", pathToCopy, file.Name())
+		filePathToWrite := fmt.Sprintf("%s/%s", pathToCopyTo, file.Name())
 
 		if file.IsDir() {
-			err = CopyStaticDirectory(
-				fmt.Sprintf("%s/%s", pathToCopy, file.Name()),
-				fmt.Sprintf("%s/%s", pathToCopyTo, file.Name()),
-			)
+			err = sccp.CopyStaticDirectory(filePathToRead, filePathToWrite)
 			if err != nil {
 				return err
 			}
 		} else {
-			filePathToRead := fmt.Sprintf("%s/%s", pathToCopy, file.Name())
-			fileBytes, err := staticFiles.ReadFile(filePathToRead)
+			adjustmentIdx := sccp.doesFilePathNeedChanging(filePathToRead)
+			if adjustmentIdx > -1 {
+				alteration := sccp.filePathAlterations[adjustmentIdx]
 
-			if err != nil {
-				fileBytes, err = os.ReadFile(filePathToRead)
-
-				if err != nil {
-					return err
+				if alteration.filePathToWrite != "" {
+					filePathToWrite = alteration.filePathToWrite
 				}
 			}
 
-			filePath := fmt.Sprintf("%s/%s", pathToCopyTo, file.Name())
-
-			err = os.WriteFile(
-				filePath,
-				fileBytes,
-				FILE_WRITE_PERMS,
-			)
+			err = sccp.CopyStaticFile(filePathToRead, filePathToWrite)
 
 			if err != nil {
-				if os.IsExist(err) {
-					log.Printf("Attempting to overwrite file at %s, skipping\n", filePath)
-				} else {
-					return err
-				}
+				return err
 			}
 		}
 	}
 
 	return nil
+}
+
+func (sccp *StaticCodeCopyPlan) CopyStaticFile(
+	filePathToRead string,
+	filePathToWrite string,
+) error {
+	fileBytes, err := staticFiles.ReadFile(filePathToRead)
+	if err != nil {
+		fileBytes, err = os.ReadFile(filePathToRead)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	adjustmentIdx := sccp.doesFilePathNeedChanging(filePathToRead)
+	if adjustmentIdx > -1 {
+		adjustment := sccp.filePathAlterations[adjustmentIdx]
+		fileBytes = sccp.performFileAdjustments(fileBytes, adjustment)
+	}
+
+	err = writeFile(filePathToWrite, fileBytes)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func writeFile(filePathToWrite string, fileBytes []byte) error {
+	err := os.WriteFile(filePathToWrite, fileBytes, FILE_WRITE_PERMS)
+
+	if err != nil {
+		if os.IsExist(err) {
+			log.Printf("Attempting to overwrite file at %s, skipping\n", filePathToWrite)
+		} else {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (sccp *StaticCodeCopyPlan) performFileAdjustments(
+	fileBytes []byte,
+	alteration StaticCodeFilePathAlteration,
+) []byte {
+	newFileString := ""
+
+	if alteration.stringToReadForReplace != "" && alteration.stringToWriteForReplace != "" {
+		newFileString = strings.ReplaceAll(
+			string(fileBytes),
+			alteration.stringToReadForReplace,
+			alteration.stringToWriteForReplace,
+		)
+	}
+
+	return []byte(newFileString)
+}
+
+func (sccp *StaticCodeCopyPlan) doesFilePathNeedChanging(filePath string) int {
+	for i := 0; i < len(sccp.filePathAlterations); i++ {
+		if filePath == sccp.filePathAlterations[i].filePathToRead {
+			return i
+		}
+	}
+
+	return -1
 }

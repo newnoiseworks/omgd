d60596cda3afa326920875dca7b801fc894909d7
New Noise Anand <amallik@newnoiseworks.com>
2023-02-07 01:46:07 -0500

    passing tests on code generation for channel

--- utils/code.go
+++ utils/code.go
@@ -23,6 +23,8 @@ func (cp *CodeGenerationPlan) Generate() {
 		cp.generateNew()
 	case "example-2d-player-movement":
 		cp.generateExample2DPlayerMovement()
+	case "channel":
+		cp.generateChannel()
 	}
 
 	// optionally skip cleanup to observe files, mostly for testing
@@ -34,7 +36,10 @@ func (cp *CodeGenerationPlan) Generate() {
 // generates code needed for new projects
 func (cp *CodeGenerationPlan) generateNew() {
 	outputPath := fmt.Sprintf("%s/%s", cp.OutputDir, cp.Target)
-	err := CopyStaticDirectory("static/new", outputPath)
+
+	sccp := StaticCodeCopyPlan{}
+
+	err := sccp.CopyStaticDirectory("static/new", outputPath)
 
 	if err != nil {
 		log.Fatal(err)
@@ -50,7 +55,7 @@ func (cp *CodeGenerationPlan) generateNew() {
 	BuildTemplatesFromPath(
 		fmt.Sprintf("%s/profiles/local", outputPath),
 		outputPath,
-		"newomgdtpl",
+		"omgdtpl",
 		!cp.SkipCleanup,
 		cp.Verbosity,
 	)
@@ -60,7 +65,71 @@ func (cp *CodeGenerationPlan) generateNew() {
 func (cp *CodeGenerationPlan) generateExample2DPlayerMovement() {
 	tmpDir := fmt.Sprintf("%s/.omgdtmp", cp.OutputDir)
 
-	err := CopyStaticDirectory("static/example-2d-player-movement", tmpDir)
+	sccp := StaticCodeCopyPlan{}
+
+	err := sccp.CopyStaticDirectory("static/example-2d-player-movement", tmpDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	newProfile := GetProfile(fmt.Sprintf("%s/profiles/local", tmpDir))
+	newProfile.UpdateProfile("omgd.channel_name", cp.Target)
+
+	BuildTemplatesFromPath(
+		fmt.Sprintf("%s/profiles/local", tmpDir),
+		tmpDir,
+		"omgdtpl",
+		true,
+		cp.Verbosity,
+	)
+}
+
+// generates channel management code
+func (cp *CodeGenerationPlan) generateChannel() {
+	tmpDir := fmt.Sprintf("%s/.omgdtmp", cp.OutputDir)
+	staticDir := "static/channel"
+
+	// TODO: There should probably be a check to make sure the
+	// user passes in a snake_case_channel_name meaning no caps
+	// and no spaces just underscores, shouldn't be too hard
+
+	snakeChannel := cp.Target
+	camelChannel := StrToCamel(cp.Target)
+
+	sccp := StaticCodeCopyPlan{
+		filePathAlterations: []StaticCodeFilePathAlteration{
+			{
+				filePathToRead:  fmt.Sprintf("%s/game/Autoloads/ChannelManager.gd.omgdtpl", staticDir),
+				filePathToWrite: fmt.Sprintf("%s/game/Autoloads/%sManager.gd.omgdtpl", tmpDir, camelChannel),
+			},
+			{
+				filePathToRead:  fmt.Sprintf("%s/game/Autoloads/ChannelEvent.gd.tmpl.omgdtpl", staticDir),
+				filePathToWrite: fmt.Sprintf("%s/game/Autoloads/%sEvent.gd.tmpl.omgdtpl", tmpDir, camelChannel),
+			},
+			{
+				filePathToRead:  fmt.Sprintf("%s/resources/channel_events.yml.omgdtpl", staticDir),
+				filePathToWrite: fmt.Sprintf("%s/resources/%s_events.yml.omgdtpl", tmpDir, snakeChannel),
+			},
+			{
+				filePathToRead:  fmt.Sprintf("%s/game/RootScenes/ChannelMUD.tscn.omgdtpl", staticDir),
+				filePathToWrite: fmt.Sprintf("%s/game/RootScenes/%sMUD.tscn.omgdtpl", tmpDir, camelChannel),
+			},
+			{
+				filePathToRead:  fmt.Sprintf("%s/game/RootScenes/ChannelMUDController.gd.omgdtpl", staticDir),
+				filePathToWrite: fmt.Sprintf("%s/game/RootScenes/%sMUDController.gd.omgdtpl", tmpDir, camelChannel),
+			},
+			{
+				filePathToRead:  fmt.Sprintf("%s/server/nakama/data/modules/channel.lua.omgdtpl", staticDir),
+				filePathToWrite: fmt.Sprintf("%s/server/nakama/data/modules/%s.lua.omgdtpl", tmpDir, snakeChannel),
+			},
+			{
+				filePathToRead:  fmt.Sprintf("%s/server/nakama/data/modules/channel_manager.lua.omgdtpl", staticDir),
+				filePathToWrite: fmt.Sprintf("%s/server/nakama/data/modules/%s_manager.lua.omgdtpl", tmpDir, snakeChannel),
+			},
+		},
+	}
+
+	err := sccp.CopyStaticDirectory("static/channel", tmpDir)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -83,6 +152,8 @@ func (cp *CodeGenerationPlan) Cleanup() {
 	// case "new":
 	case "example-2d-player-movement":
 		cp.cleanupExample2DPlayerMovement()
+	case "channel":
+		cp.cleanupChannel()
 	}
 }
 
@@ -95,7 +166,9 @@ func (cp *CodeGenerationPlan) cleanupExample2DPlayerMovement() {
 		log.Fatal(err)
 	}
 
-	err = CopyStaticDirectory(tmpDir, cp.OutputDir)
+	sccp := StaticCodeCopyPlan{}
+
+	err = sccp.CopyStaticDirectory(tmpDir, cp.OutputDir)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -104,9 +177,29 @@ func (cp *CodeGenerationPlan) cleanupExample2DPlayerMovement() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
 
-	// TODO: use without this, then if something goes wrong, try it
-	// 3. build templates across entire project (for some reason? not sure how to test / prove this / why it happens tbh)
+// cleans up channel code
+func (cp *CodeGenerationPlan) cleanupChannel() {
+	tmpDir := fmt.Sprintf("%s/.omgdtmp", cp.OutputDir)
+
+	err := os.RemoveAll(fmt.Sprintf("%s/profiles", tmpDir))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sccp := StaticCodeCopyPlan{}
+
+	err = sccp.CopyStaticDirectory(tmpDir, cp.OutputDir)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = os.RemoveAll(tmpDir)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 // cleans up and resets omgdtmp dir

3f524b9db94af99c58e0daa6c29e054e444d99f0
New Noise Anand <amallik@newnoiseworks.com>
2023-02-22 11:31:34 -0500

    sets up tests for infra util lib

---
+++ utils/infra_test.go
@@ -0,0 +1,87 @@
+package utils
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestDeployInfra(t *testing.T) {
+	testDir := "static/test/infra_test_dir"
+
+	t.Cleanup(func() {
+		err := os.RemoveAll(fmt.Sprintf("%s/.omgdtmp", testDir))
+
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	DeployInfra("staging", testCmdOnDir, true)
+
+	// 1. Should create or empty .omgdtmp directory to work in
+	testFileShouldExist(t, fmt.Sprintf("%s/.omgdtmp", testDir))
+
+	// 2. Should clone repo at base of dir (? how to test w/o submodules? clone entire base repo maybe?)
+	testFileShouldExist(t, fmt.Sprintf("%s/.omgdtmp/game", testDir))
+	testFileShouldExist(t, fmt.Sprintf("%s/.omgdtmp/server", testDir))
+	testFileShouldExist(t, fmt.Sprintf("%s/.omgdtmp/.git", testDir))
+	testFileShouldExist(t, fmt.Sprintf("%s/.omgdtmp/profiles", testDir))
+
+	// 3. Copy profiles directory into new .omgdtmp dir (add staging.yml to static/test/infraDir)
+	testFileShouldExist(t, fmt.Sprintf("%s/.omgdtmp/profiles/staging.yml", testDir))
+
+	// 4. Build profiles directory in new .omgdtmp dir
+	testFileShouldExist(t, fmt.Sprintf("%s/.omgdtmp/.omgd/staging.yml", testDir))
+
+	testCmdOnDirValidResponseSet = []testCmdOnDirResponse{
+		{
+			cmdStr:  "omgd run --profile=.omgd/staging",
+			cmdDesc: "",
+			cmdDir:  fmt.Sprintf("%s/.omgdtmp/", testDir),
+		},
+	}
+
+	// 5. Run main task in new .omgdtmp dir profiles/profile.yml file
+	testCmdOnDirValidCmdSet(t, "DeployInfra")
+}
+
+func TestDestroyInfra(t *testing.T) {
+	testDir := "static/test/infra_test_dir"
+
+	t.Cleanup(func() {
+		err := os.RemoveAll(fmt.Sprintf("%s/.omgdtmp", testDir))
+
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	DestroyInfra("staging", testCmdOnDir, true)
+
+	// 1. Should create or empty .omgdtmp directory to work in
+	testFileShouldExist(t, fmt.Sprintf("%s/.omgdtmp", testDir))
+
+	// 2. Should clone repo at base of dir (? how to test w/o submodules? clone entire base repo maybe?)
+	testFileShouldExist(t, fmt.Sprintf("%s/.omgdtmp/game", testDir))
+	testFileShouldExist(t, fmt.Sprintf("%s/.omgdtmp/server", testDir))
+	testFileShouldExist(t, fmt.Sprintf("%s/.omgdtmp/.git", testDir))
+	testFileShouldExist(t, fmt.Sprintf("%s/.omgdtmp/profiles", testDir))
+
+	// 3. Copy profiles directory into new .omgdtmp dir (add staging.yml to static/test/infraDir)
+	testFileShouldExist(t, fmt.Sprintf("%s/.omgdtmp/profiles/staging.yml", testDir))
+
+	// 4. Build profiles directory in new .omgdtmp dir
+	testFileShouldExist(t, fmt.Sprintf("%s/.omgdtmp/.omgd/staging.yml", testDir))
+
+	testCmdOnDirValidResponseSet = []testCmdOnDirResponse{
+		{
+			cmdStr:  "omgd run --profile=.omgd/staging",
+			cmdDesc: "",
+			cmdDir:  fmt.Sprintf("%s/.omgdtmp/", testDir),
+		},
+	}
+
+	// 5. Run destroy-infra task in new .omgdtmp dir profiles/profile.yml file
+	testCmdOnDirValidCmdSet(t, "DestroyInfra")
+}

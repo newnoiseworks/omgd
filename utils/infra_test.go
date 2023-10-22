13d9f0362859bc57c7e10d0cc3f8ee9dae3b2045
Anand "New Noise" Mallik <amallik@newnoiseworks.com>
2023-10-22 15:57:47 -0400

    adds tfvars template to execute, removing copyToStaticDir from test, eventually code

--- utils/infra_test.go
+++ utils/infra_test.go
@@ -10,79 +10,18 @@ func TestDeployInfra(t *testing.T) {
 	testDir := "static/test/infra_test_dir"
 
 	t.Cleanup(func() {
-		err := os.RemoveAll(fmt.Sprintf("%s/.omgdtmp", testDir))
+		err := os.RemoveAll(fmt.Sprintf("%s/.omgd", testDir))
 
 		if err != nil {
 			t.Fatal(err)
 		}
 
-		testCmdOnDirResponses = []testCmdOnDirResponse{}
-	})
-
-	profile := GetProfileFromDir("profiles/staging.yml", testDir)
-
-	infraChange := InfraChange{
-		OutputDir:    "static/test/infra_test_dir",
-		Profile:      profile,
-		CmdOnDir:     testCmdOnDir,
-		Verbosity:    false,
-		CopyToTmpDir: true,
-	}
-
-	infraChange.DeployInfra()
-
-	// 1. Should create or empty .omgdtmp directory to work in
-	testFileShouldExist(t, fmt.Sprintf("%s/.omgdtmp", testDir))
-
-	// 2. Should clone repo at base of dir (? how to test w/o submodules? clone entire base repo maybe?)
-	testFileShouldExist(t, fmt.Sprintf("%s/.omgdtmp/game", testDir))
-	testFileShouldExist(t, fmt.Sprintf("%s/.omgdtmp/server", testDir))
-	testFileShouldExist(t, fmt.Sprintf("%s/.omgdtmp/profiles", testDir))
-
-	// 3. Copy profiles directory into new .omgdtmp dir (add staging.yml to static/test/infraDir)
-	testFileShouldExist(t, fmt.Sprintf("%s/.omgdtmp/profiles/staging.yml", testDir))
-
-	// 4. Build profiles directory in new .omgdtmp dir
-	testFileShouldExist(t, fmt.Sprintf("%s/.omgdtmp/.omgd/staging.yml", testDir))
-
-	// 5. Build templates should execute
-	testFileShouldExist(t, fmt.Sprintf("%s/.omgdtmp/.omgd/server/infra/gcp/terraform.tfvars", testDir))
-
-	cmdDirStr := fmt.Sprintf("%s/.omgdtmp", testDir)
-	cmdDirStrTf := fmt.Sprintf("%s/.omgd/server/infra/gcp/", cmdDirStr)
-
-	testCmdOnDirValidResponseSet = []testCmdOnDirResponse{
-		{
-			cmdStr:    "terraform init",
-			cmdDesc:   "setting up terraform locally",
-			cmdDir:    cmdDirStrTf,
-			verbosity: false,
-		},
-		{
-			cmdStr:    "terraform apply -auto-approve",
-			cmdDesc:   "updating cloud infra if needed",
-			cmdDir:    cmdDirStrTf,
-			verbosity: false,
-		},
-		{
-			cmdStr:    "terraform output -raw server_ip",
-			cmdDesc:   "getting ip of newly created server...",
-			cmdDir:    cmdDirStrTf,
-			verbosity: false,
-		},
-	}
-	// 5. Run main task in new .omgdtmp dir profiles/profile.yml file
-	testCmdOnDirValidCmdSet(t, "DeployInfra")
-
-	testForFileAndRegexpMatch(t, fmt.Sprintf("%s/.omgdtmp/profiles/staging.yml", testDir), "127.6.6.6")
-	testForFileAndRegexpMatch(t, fmt.Sprintf("%s/.omgdtmp/.omgd/staging.yml", testDir), "127.6.6.6")
-}
-
-func TestDeployInfraWithoutCopying(t *testing.T) {
-	testDir := "static/test/infra_test_dir"
-
-	t.Cleanup(func() {
-		err := os.RemoveAll(fmt.Sprintf("%s/.omgd", testDir))
+		err = os.RemoveAll(
+			fmt.Sprintf(
+				"%s/server/infra/gcp/terraform.tfvars",
+				testDir,
+			),
+		)
 
 		if err != nil {
 			t.Fatal(err)
@@ -90,7 +29,7 @@ func TestDeployInfraWithoutCopying(t *testing.T) {
 
 		profile := GetProfileFromDir("profiles/staging.yml", testDir)
 
-		profile.UpdateProfile("omgd.deploy.server.gcloud.host", "")
+		profile.UpdateProfile("omgd.deploy.server.gcloud.host", "???")
 
 		testCmdOnDirResponses = []testCmdOnDirResponse{}
 	})
@@ -121,7 +60,10 @@ func TestDeployInfraWithoutCopying(t *testing.T) {
 	// 4. Build profiles directory in new .omgdtmp dir
 	testFileShouldExist(t, fmt.Sprintf("%s/.omgd/staging.yml", testDir))
 
-	cmdDirStrTf := fmt.Sprintf("%s/.omgd/server/infra/gcp/", testDir)
+	// 5. BuildTemplates runs
+	testFileShouldExist(t, fmt.Sprintf("%s/server/infra/gcp/terraform.tfvars", testDir))
+
+	cmdDirStrTf := fmt.Sprintf("%s/server/infra/gcp/", testDir)
 
 	testCmdOnDirValidResponseSet = []testCmdOnDirResponse{
 		{

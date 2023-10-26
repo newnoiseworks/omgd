0c0cd496d2d293facd43cca9ab9bd9c2d9780cdb
Anand "New Noise" Mallik <amallik@newnoiseworks.com>
2023-10-26 19:21:34 -0400

    lowercase is private enough

--- utils/logger.go
+++ utils/logger.go
@@ -18,68 +18,68 @@ const (
 	TRACE_LOG LogLevel = iota
 )
 
-// Logger._logFn used internally for logging, SetLogFn adjusts for tests
-var _logFn = log.Println
+// Logger.logFn used internally for logging, SetLogFn adjusts for tests
+var logFn = log.Println
 
-// Sets Logger._logFn for tests
+// Sets Logger.logFn for tests
 func SetLogFn(logFn func(v ...any)) {
-	_logFn = logFn
+	logFn = logFn
 }
 
-// Logger._printFn used internally for logging, SetPrintFn adjusts for tests
-var _printFn = fmt.Println
+// Logger.printFn used internally for logging, SetPrintFn adjusts for tests
+var printFn = fmt.Println
 
-// Sets Logger._printFn for tests
+// Sets Logger.printFn for tests
 func SetPrintFn(printFn func(v ...any) (n int, err error)) {
-	_printFn = printFn
+	printFn = printFn
 }
 
 // Contains log level according to environment var, adjusted for tests
-var _envLogLevel LogLevel
+var envLogLevel LogLevel
 
 // Sets environment log level variable, used for tests
 func SetEnvLogLevel(logLevel LogLevel) {
-	_envLogLevel = logLevel
+	envLogLevel = logLevel
 }
 
-var _envLogOutputWithTimestamp bool
+var envLogOutputWithTimestamp bool
 
 func init() {
 	switch os.Getenv("OMGD_LOG_LEVEL") {
 	case "FATAL":
-		_envLogLevel = FATAL_LOG
+		envLogLevel = FATAL_LOG
 		break
 	case "ERROR":
-		_envLogLevel = ERROR_LOG
+		envLogLevel = ERROR_LOG
 		break
 	case "WARN":
-		_envLogLevel = WARN_LOG
+		envLogLevel = WARN_LOG
 		break
 	case "INFO":
-		_envLogLevel = INFO_LOG
+		envLogLevel = INFO_LOG
 		break
 	case "DEBUG":
-		_envLogLevel = DEBUG_LOG
+		envLogLevel = DEBUG_LOG
 		break
 	case "TRACE":
-		_envLogLevel = TRACE_LOG
+		envLogLevel = TRACE_LOG
 		break
 	default:
-		_envLogLevel = WARN_LOG
+		envLogLevel = WARN_LOG
 		break
 	}
 
 	timestampVal, timestampValSet := os.LookupEnv("OMGD_LOG_WITH_TIMESTAMP")
 
 	if timestampValSet && timestampVal != "false" {
-		_envLogOutputWithTimestamp = true
+		envLogOutputWithTimestamp = true
 	} else {
-		_envLogOutputWithTimestamp = false
+		envLogOutputWithTimestamp = false
 	}
 }
 
 func LogFatal(message string) {
-	if _envLogLevel >= DEBUG_LOG {
+	if envLogLevel >= DEBUG_LOG {
 		debug.PrintStack()
 	}
 
@@ -108,16 +108,16 @@ func LogTrace(message string) {
 }
 
 func processMessageAgainstLogLevel(message string, logLevel LogLevel) {
-	if logLevel <= _envLogLevel {
+	if logLevel <= envLogLevel {
 		outputLogMessage(message)
 	}
 }
 
 func outputLogMessage(message string) {
-	if _envLogOutputWithTimestamp {
-		_logFn(message)
+	if envLogOutputWithTimestamp {
+		logFn(message)
 	} else {
-		_, err := _printFn(message)
+		_, err := printFn(message)
 
 		if err != nil {
 			debug.PrintStack()

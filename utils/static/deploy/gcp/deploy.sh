6f47025fc483f548157d7fe2f1f77bd5ab70fc42
Anand "New Noise" Mallik <83200798+newnoiseanand@users.noreply.github.com>
2023-12-12 23:05:06 -0500

    deploy script makes all commands with a user omgd-sa (#264)

--- utils/static/deploy/gcp/deploy.sh
+++ utils/static/deploy/gcp/deploy.sh
@@ -2,16 +2,16 @@
 
 export CLOUDSDK_AUTH_CREDENTIAL_FILE_OVERRIDE=~/.config/gcloud/application_default_credentials.json
 
-gcloud compute ssh --project ${GCP_PROJECT} --zone ${GCP_ZONE} --command "sudo curl -sSL https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-linux-x86_64 -o /var/lib/google/docker-compose && sudo chmod o+x /var/lib/google/docker-compose && mkdir -p ~/.docker/cli-plugins && sudo ln -sf /var/lib/google/docker-compose ~/.docker/cli-plugins/docker-compose" ${OMGD_PROJECT}-omgd-dev-instance-${OMGD_PROFILE}
+gcloud compute ssh --project ${GCP_PROJECT} --zone ${GCP_ZONE} --command "sudo curl -sSL https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-linux-x86_64 -o /var/lib/google/docker-compose && sudo chmod o+x /var/lib/google/docker-compose && mkdir -p ~/.docker/cli-plugins && sudo ln -sf /var/lib/google/docker-compose ~/.docker/cli-plugins/docker-compose" omgd-sa@${OMGD_PROJECT}-omgd-dev-instance-${OMGD_PROFILE}
 
-gcloud compute ssh --project ${GCP_PROJECT} --zone ${GCP_ZONE} --command "truncate -s 0 /var/log/docker.log" ${OMGD_PROJECT}-omgd-dev-instance-${OMGD_PROFILE}
+gcloud compute ssh --project ${GCP_PROJECT} --zone ${GCP_ZONE} --command "truncate -s 0 /var/log/docker.log" omgd-sa@${OMGD_PROJECT}-omgd-dev-instance-${OMGD_PROFILE}
 
 # scp'ing the docker-compose file in case it's the first deploy and we don't have one, so the subsequent down command won't error out
-gcloud compute scp --project ${GCP_PROJECT} --zone ${GCP_ZONE} --force-key-file-overwrite ../../../servers/docker-compose.yml ${OMGD_PROJECT}-omgd-dev-instance-${OMGD_PROFILE}:
+gcloud compute scp --project ${GCP_PROJECT} --zone ${GCP_ZONE} --force-key-file-overwrite ../../../servers/docker-compose.yml omgd-sa@${OMGD_PROJECT}-omgd-dev-instance-${OMGD_PROFILE}:
 
-gcloud compute ssh --project ${GCP_PROJECT} --zone ${GCP_ZONE} --command "docker compose down" ${OMGD_PROJECT}-omgd-dev-instance-${OMGD_PROFILE}
+gcloud compute ssh --project ${GCP_PROJECT} --zone ${GCP_ZONE} --command "docker compose down" omgd-sa@${OMGD_PROJECT}-omgd-dev-instance-${OMGD_PROFILE}
 
-gcloud compute scp --project ${GCP_PROJECT} --zone ${GCP_ZONE} --recurse --force-key-file-overwrite ../../../servers/* ${OMGD_PROJECT}-omgd-dev-instance-${OMGD_PROFILE}:
+gcloud compute scp --project ${GCP_PROJECT} --zone ${GCP_ZONE} --recurse --force-key-file-overwrite ../../../servers/* omgd-sa@${OMGD_PROJECT}-omgd-dev-instance-${OMGD_PROFILE}:
 
-gcloud compute ssh --project ${GCP_PROJECT} --zone ${GCP_ZONE} --command "docker compose up -d ${OMGD_SERVER_SERVICES}" ${OMGD_PROJECT}-omgd-dev-instance-${OMGD_PROFILE}
+gcloud compute ssh --project ${GCP_PROJECT} --zone ${GCP_ZONE} --command "docker compose up -d ${OMGD_SERVER_SERVICES}" omgd-sa@${OMGD_PROJECT}-omgd-dev-instance-${OMGD_PROFILE}
 

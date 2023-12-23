4c5a5e2ed6c5cd15be62a5adab292eeebea88d0b
Anand "New Noise" Mallik <amallik@newnoiseworks.com>
2023-12-22 21:31:16 -0500

    proper execution command for windows

--- utils/servers.go
+++ utils/servers.go
@@ -64,7 +64,7 @@ func (serversChange *ServersChange) Deploy() {
 	deployCmd := "./deploy.sh"
 
 	if runtime.GOOS == "windows" {
-		deployCmd = "deploy.bat"
+		deployCmd = "cmd.exe /C deploy.bat"
 	}
 
 	serversChange.CmdOnDirWithEnv(
